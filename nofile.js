4588cc1da804a8bb81ed2f2cb61a702548a7eaaa
Yad Smood <y.s.inside@gmail.com>
2015-08-29 15:42:16 +0800

    upd: nofile

--- nofile.js
+++ nofile.js
@@ -3,25 +3,43 @@
 var kit = require("nokit");
 
 module.exports = function (task, option) {
-    option("-n <basic>", "example name", "basic");
+    function enableES7 (args) {
+        return [
+            "--optional",
+            "es7.asyncFunctions"
+        ].concat(args);
+    }
 
-    task("default", "run test", function (opts) {
+    option("-n <basic>", "example file name", "basic");
+    task("default", "run an example", function (opts) {
         kit.monitorApp({
             bin: "babel-node",
-            args: [
-                "examples/" + opts.N,
-                "--optional", "es7.asyncFunctions"
-            ]
+            args: enableES7(["examples/" + opts.N])
         });
     });
 
-    task("test-api", "run test api", function (opts) {
-        kit.monitorApp({
-            bin: "babel-node",
-            args: [
-                "test/" + opts.N,
-                "whitelist", "es7.asyncFunctions es6.templateLiterals es6.destructuring es6.arrowFunctions"
-            ]
+    task("watch-test", "run & watch test api", function () {
+        kit.watchDir("test", {
+            patterns: "test/**/*.js",
+            handler: function (type, path) {
+                if (type === "delete") return;
+                kit.spawn("babel-node", enableES7([path]));
+            }
+        });
+    });
+
+    task("lint", "lint all code of this project", function () {
+        function lint (f) {
+            f.set(null);
+            return kit.spawn("eslint", [f.path]);
+        }
+
+        return kit.warp("{examples,lib,test}/**/*.js").load(lint).run();
+    });
+
+    task("test", ["lint"], "run test once", function () {
+        return kit.globSync("test/**/*.js").map(function (path) {
+            return kit.spawn("babel-node", enableES7([path]));
         });
     });
 };
