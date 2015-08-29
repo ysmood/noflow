9474f4df643f37cce911641fc601432c11099bf1
Yad Smood <y.s.inside@gmail.com>
2015-08-29 14:27:08 +0800

    opt: format code

--- test/flow.api.js
+++ test/flow.api.js
@@ -1,58 +1,53 @@
-var path = require('path'),
-	http = require('http'),
-	Promise = require('yaku'),
- 	kit = require("nokit");
-	 
-var flow = require(path.join(__dirname, '../lib/flow'));
-var ken = kit.require('ken');
-var testSuite = ken();
+var http = require("http"),
+    Promise = require("yaku"),
+    kit = require("nokit");
 
-// alias 
-// testfiy
-var it = testSuite;	
-	
-testSuite.sync([
-	
-	it('should proxy flow handler', () => {
-		var server = http.createServer(flow([echoData]));
-		var listener = kit.promisify(server.listen, server);
-		
-		return listener(0).then(() => {
-			return kit.request({
-				url: `http://127.0.0.1:${server.address().port}`,
-				reqData: 'test'
-			});
-		}).then((body) => {
-			ken.eq(body, 'echo: test');
-		}).then(() => {
-			// close server
-			server.close();
-		}, (e) => { server.close(); throw e; });
-	}),
-	
-	it('should proxy flow string middleware', () => {
-		
-		var server = http.createServer(flow(['string works']));
-		var listener = kit.promisify(server.listen, server);
-		
-		return listener(0).then(() => {
-			return kit.request({
-				url: `http://127.0.0.1:${server.address().port}`
-			});
-		}).then((body) => {
-			ken.eq('string works', body);
-		}).then(() => {
-			// close server
-			server.close();
-		}, (e) => { server.close(); throw e; });
-	})
+var flow = require("../lib/flow");
+var ken = kit.require("ken");
+var it = ken();
+
+it.sync([
+
+    it("should proxy flow handler", () => {
+        var server = http.createServer(flow([echoData]));
+        var listener = kit.promisify(server.listen, server);
+
+        return listener(0).then(() => {
+            return kit.request({
+                url: `http://127.0.0.1:${server.address().port}`,
+                reqData: "test"
+            });
+        }).then((body) => {
+            ken.eq(body, "echo: test");
+        }).then(() => {
+            // close server
+            server.close();
+        }, (e) => { server.close(); throw e; });
+    }),
+
+    it("should proxy flow string middleware", () => {
+
+        var server = http.createServer(flow(["string works"]));
+        var listener = kit.promisify(server.listen, server);
+
+        return listener(0).then(() => {
+            return kit.request({
+                url: `http://127.0.0.1:${server.address().port}`
+            });
+        }).then((body) => {
+            ken.eq("string works", body);
+        }).then(() => {
+            // close server
+            server.close();
+        }, (e) => { server.close(); throw e; });
+    })
 ]);
 
-var echoData = function _echoData(ctx) {
-	return new Promise((fulfill) => {
-		ctx.req.on('data', (data) => {
-			ctx.body = 'echo: ' + data;
-			fulfill();
-		});
-	});
+function echoData (ctx) {
+    return new Promise((fulfill) => {
+        ctx.req.on("data", (data) => {
+            ctx.body = "echo: " + data;
+            fulfill();
+        });
+    });
 }
