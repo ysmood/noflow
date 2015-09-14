ea4143d18cf2ca966be6c985856a53a5d2c94007
Yad Smood <y.s.inside@gmail.com>
2015-09-15 00:59:16 +0800

    upd: nofile with es7

--- nofile.js
+++ nofile.js
@@ -1,42 +1,40 @@
 "use strict";
 
-var kit = require("nokit");
-var _ = kit._;
-var br = kit.require("brush");
+import kit from "nokit";
+let { _ } = kit;
+let br = kit.require("brush");
 
-module.exports = function (task, option) {
-    var enableES7 = _.curry(_.union, 2)(["--optional", "es7.asyncFunctions"]);
-
+module.exports = (task, option) => {
     option("-t <.*>", "unit test regex filter", ".*");
     function test (pattern) {
         return kit.spawn(
             "babel-node",
-            enableES7(["test/index.js"]),
+            ["test/index.js"],
             { env: _.assign(process.env, { pattern: pattern }) }
         );
     }
 
     option("-n <examples/basic>", "example file name", "examples/basic");
-    task("default", "run an example", function (opts) {
+    task("default", "run an example", (opts) => {
         kit.monitorApp({
             bin: "babel-node",
-            args: enableES7([opts.N]),
+            args: [opts.N],
             watchList: ["examples/**/*.js", "src/**/*.js"]
         });
     });
 
-    task("build-docs", "build readme.md", function () {
+    task("build-docs", "build readme.md", () => {
         return kit.warp("src/**/*.js")
         .load(
             kit.drives.comment2md({ h: 2, tpl: "doc/readme.jst.md" })
         ).run();
     });
 
-    task("build", ["build-docs"], "build src from es6 to es5", function () {
+    task("build", ["build-docs"], "build src from es6 to es5", () => {
         return kit.spawn("babel", ["src", "--out-dir", "lib"]);
     });
 
-    task("watch-test", "run & watch test api", function (opts) {
+    task("watch-test", "run & watch test api", (opts) => {
         function handler (path, curr, prev, isDel) {
             kit.logs(br.cyan("modifed:"), path);
             kit.logs(br.cyan("***** run unit tests *****"));
@@ -47,7 +45,7 @@ module.exports = function (task, option) {
         kit.watchFiles("{test,src}/**/*.js", { handler: handler });
     });
 
-    task("lint", "lint all code of this project", function () {
+    task("lint", "lint all code of this project", () => {
         function lint (f) {
             f.set(null);
             return kit.spawn("eslint", [f.path]);
@@ -56,24 +54,22 @@ module.exports = function (task, option) {
         return kit.warp("{examples,src,test}/**/*.js").load(lint).run();
     });
 
-    task("test", ["lint"], "run test once", function () {
-        test(".*").catch(function (res) {
+    task("test", ["lint"], "run test once", () => {
+        test(".*").catch((res) => {
             process.exit(res.code);
         });
     });
 
-    task("benchmark", "run benchmark", function () {
+    task("benchmark", "run benchmark", () => {
         var paths = kit.globSync("benchmark/basic/*.js");
         var port = 3120;
-        return kit.flow(paths.reverse().map(function (path) {
-            return function () {
+        return kit.flow(paths.reverse().map((path) => {
+            return async () => {
                 var p = port++;
                 var name = kit.path.basename(path, ".js");
                 var child = kit.spawn("node", [path, p]).process;
-                return kit.spawn("node", ["benchmark/index.js", p, name])
-                .then(function () {
-                    child.kill();
-                });
+                await kit.spawn("node", ["benchmark/index.js", p, name]);
+                child.kill();
             };
         }))();
     });
