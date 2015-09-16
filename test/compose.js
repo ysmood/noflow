90b9efaf5f2f5ee327cc6a73c288b2d20e667c90
ddchen <842914439@qq.com>
2015-09-16 09:42:14 +0800

    order test for noflow

--- test/compose.js
+++ test/compose.js
@@ -52,6 +52,68 @@ export default ({
         );
 
         return eq(await request(app)(), "catch err");
+    }),
+
+    it("order by decreasing time", async () => {
+        let app = flow();
+
+        let c = "";
+
+        let p1 = new Promise((resolve) => {
+                    setTimeout(() => resolve("p1"), 200);
+                });
+
+        let p2 = new Promise((resolve) => {
+                    setTimeout(() => resolve("p2"), 100);
+                });
+
+        app.push(
+            async ($) => {
+                c += await p1;
+                await $.next();
+            },
+            async ($) =>{
+                c += await p2;
+                await $.next();
+            },
+            $ => {
+                $.body = c;
+            }
+        );
+
+        return eq(await request(app)(), "p1p2");
+
+    }),
+
+    it("order by increasing time", async () => {
+        let app = flow();
+
+        let c = "";
+
+        let p1 = new Promise((resolve) => {
+                    setTimeout(() => resolve("p1"), 100);
+                });
+
+        let p2 = new Promise((resolve) => {
+                    setTimeout(() => resolve("p2"), 200);
+                });
+
+        app.push(
+            async ($) => {
+                c += await p1;
+                await $.next();
+            },
+            async ($) => {
+                c += await p2;
+                await $.next();
+            },
+            $ => {
+                $.body = c;
+            }
+        );
+
+        return eq(await request(app)(), "p1p2");
+
     })
 
 ];
