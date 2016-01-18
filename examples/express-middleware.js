e7db20385a07d9f1ff53ff7025c2d95fe242d9c3
Yad Smood <y.s.inside@gmail.com>
2016-01-18 13:11:18 +0800

    opt: example

--- examples/express-middleware.js
+++ examples/express-middleware.js
@@ -1,22 +1,12 @@
 import flow from "../src";
 import bodyParser from "body-parser";
+import kit from "nokit";
+let { midToFlow } = kit.require("proxy");
 
 let app = flow();
 
-// Here we create a convert to convert normal express-middleware to a
-// noflow middleware.
-let convert = (h) => ({ req, res, next }) =>
-    new Promise((resolve, reject) =>
-        h(req, res, (err) => {
-            if (err)
-                return reject(err);
-            else
-                return next().then(resolve, reject);
-        })
-    );
-
 app.push(
-    convert(bodyParser.json()),
+    midToFlow(bodyParser.json()),
     $ => {
         $.body = $.req.body;
     }
